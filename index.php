1705be276580868ad5c6a43a75c0c36ffb6fc9f7
lasalesi <lars.mueller@libracore.com>
2017-11-21 15:38:05 +0100

    generic code generator

--- index.php
+++ index.php
@@ -3,34 +3,22 @@
  * PHP QR Code encoder
  *
  * Exemplatory usage
+ * 
+ * Make sure temp/ subfolder exists and is writeable
  *
- * PHP QR Code is distributed under LGPL 3
- * Copyright (C) 2010 Dominik Dzienia <deltalab at poczta dot fm>
- *
- * This library is free software; you can redistribute it and/or
- * modify it under the terms of the GNU Lesser General Public
- * License as published by the Free Software Foundation; either
- * version 3 of the License, or any later version.
- *
- * This library is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
- * Lesser General Public License for more details.
- *
- * You should have received a copy of the GNU Lesser General Public
- * License along with this library; if not, write to the Free Software
- * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA
+ * Refer to https://github.com/laslesi/phpqrcode.git
+ * 
  */
     
     echo "<h1>PHP QR Code</h1><hr/>";
     
     //set it to writable location, a place for temp generated PNG files
-    $PNG_TEMP_DIR = dirname(__FILE__).DIRECTORY_SEPARATOR.'temp'.DIRECTORY_SEPARATOR;
+    $PNG_TEMP_DIR = dirname(__FILE__) . DIRECTORY_SEPARATOR . 'temp' . DIRECTORY_SEPARATOR;
     
     //html PNG location prefix
     $PNG_WEB_DIR = 'temp/';
 
-    include "qrlib.php";    
+    include "lib/qrlib.php";    
     
     //ofcourse we need rights to create temp dir
     if (!file_exists($PNG_TEMP_DIR))
